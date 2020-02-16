357424d232c200b3b8a50d564d19a0f0e24fbaf1
Rocio-hub <rtlopez@outlook.es>
2020-02-16 17:58:42 +0100

    .

--- AttendanceSystem/src/gui/view/WelcomeWindowController.java
+++ AttendanceSystem/src/gui/view/WelcomeWindowController.java
@@ -7,21 +7,40 @@ package gui.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
  *
- * @author Rocío
+ * @author rtlop
  */
 public class WelcomeWindowController implements Initializable {
+
+    @FXML
+    private Button btn_student;
+    @FXML
+    private Button btn_teacher;
+    @FXML
+    private Label lbl1;
+    @FXML
+    private Label lbl2;
+    @FXML
+    private Button btn_close;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btn_student.setId("btn_student");
+        btn_teacher.setId("btn_teacher");
+        btn_close.setId("btn_close");
+        lbl1.setId("lbl1");
+        lbl2.setId("lbl2");
     }    
     
 }
