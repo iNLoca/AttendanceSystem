16f01ebb9699a46768c7f37577886d13956a4b30
Rocio-hub <rtlopez@outlook.es>
2020-02-18 19:38:03 +0100

    piecharts

--- AttendanceSystem/src/gui/controller/OverallAttendanceController.java
+++ AttendanceSystem/src/gui/controller/OverallAttendanceController.java
@@ -14,28 +14,89 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class OverallAttendanceController implements Initializable {
 
-    private PieChart pieChart;
     @FXML
     private Label lbl_totalpresence;
     @FXML
     private Label lbl_totalabsence;
     @FXML
     private Button btn_close;
+    @FXML
+    private AnchorPane anchorpaneLeft;
+    @FXML
+    private PieChart overAllChart;
+    @FXML
+    private PieChart dbosChart1;
+    @FXML
+    private PieChart dbosChart2;
+    @FXML
+    private PieChart itoChart1;
+    @FXML
+    private PieChart itoChart2;
+    @FXML
+    private PieChart scoChart1;
+    @FXML
+    private PieChart scoChart2;
+    @FXML
+    private PieChart sdeChart1;    
+    @FXML
+    private PieChart sdeChart2;
     
+   
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<PieChart.Data> pieChartData 
+        ObservableList<PieChart.Data> overallChartData
                 = FXCollections.observableArrayList(
-                new PieChart.Data("Present", 50),
-                new PieChart.Data("Absent", 50));
-//        pieChart.setData(pieChartData);
-    }    
-    
+                        new PieChart.Data("Present", 82),
+                        new PieChart.Data("Absent", 18));
+        overAllChart.setData(overallChartData);
+        
+        ObservableList<PieChart.Data> dbos1ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 30),
+                        new PieChart.Data("Absent", 70));
+        dbosChart1.setData(dbos1ChartData);
+        ObservableList<PieChart.Data> dbos2ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 23),
+                        new PieChart.Data("Absent", 77));
+        dbosChart2.setData(dbos2ChartData);
+        ObservableList<PieChart.Data> ito1ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 60),
+                        new PieChart.Data("Absent", 40));
+        itoChart1.setData(ito1ChartData);
+        ObservableList<PieChart.Data> ito2ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 50),
+                        new PieChart.Data("Absent", 50));
+        itoChart2.setData(ito2ChartData);
+        ObservableList<PieChart.Data> sco1ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 70),
+                        new PieChart.Data("Absent", 30));
+        scoChart1.setData(sco1ChartData);
+        ObservableList<PieChart.Data> sco2ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 72),
+                        new PieChart.Data("Absent", 28));
+        scoChart2.setData(sco2ChartData);
+        ObservableList<PieChart.Data> sde1ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 83),
+                        new PieChart.Data("Absent", 17));
+        sdeChart1.setData(sde1ChartData);
+        ObservableList<PieChart.Data> sde2ChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Present", 55),
+                        new PieChart.Data("Absent", 45));
+        sdeChart2.setData(sde2ChartData);
+       }
 }
