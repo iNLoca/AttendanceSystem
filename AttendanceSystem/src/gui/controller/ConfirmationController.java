e44cbef3f38b1d08270d9e4ad7f3a9dea2607a29
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2020-02-18 16:57:13 +0100

    .

--- AttendanceSystem/src/gui/controller/ConfirmationController.java
+++ AttendanceSystem/src/gui/controller/ConfirmationController.java
@@ -4,13 +4,20 @@
  * and open the template in the editor.
  */
 package gui.controller;
-
+import java.io.IOException;
 import java.net.URL;
+import java.util.EventObject;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -38,5 +45,23 @@ public class ConfirmationController implements Initializable {
         btn_view_overall_attendance.setId("btn_view_overall_attendance");
         lbl3.setId("lbl3");
     }    
+
+    @FXML
+    private void close(ActionEvent event) {
+        Stage stage = (Stage) ((Node) ((EventObject) event).getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void openOverrallAttendance(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/view/TeacherAttendanceOverview.fxml"));
+        Parent root = loader.load();
+        StudentRecordAttendanceController srctrl = loader.getController();
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
